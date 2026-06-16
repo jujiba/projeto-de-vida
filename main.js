8383c2d752d32a64efea72ff5376e42e86f48b15
jujiba <giovanna.nascimento.silva@escola.pr.gov.br>
2026-06-16 00:08:25 +0000

    aula 9

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-06-09T00:00");
-const tempoObjetivo2 = new Date("2026-06-13T00:00");
-const tempoObjetivo3 = new Date("2026-06-20T00:00");
+const tempoObjetivo1 = new Date("2026-08-09T00:00");
+const tempoObjetivo2 = new Date("2026-10-13T00:00");
+const tempoObjetivo3 = new Date("2026-07-20T00:00");
 const tempoObjetivo4 = new Date("2026-12-10T00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
@@ -42,14 +42,12 @@ function calculaTempo(tempoObjetivo){
 }
 
 function atualizaCronometro(){
-  document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-  document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-  document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-  document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-
-  for(let i=0; i<contadores.length;i++){
-    //contadores[i].textContent = calculaTempo(tempos[i]);
-  }
+for (let i = 0; i < contadores.length; i++) {
+  document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
+  document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
+  document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
+  document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
+}
 }
 
 function comecaCronometro(){
